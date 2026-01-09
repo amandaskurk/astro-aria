6e319f9a72ac70181d303a64344e4d8609268a75
amandaskurk <145383798+amandaskurk@users.noreply.github.com>
2026-01-09 12:29:57 -0700

    Add files via upload
    Restore site to version from 3 weeks ago

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://amandaskurk.github.io",
-  base: "/astro-aria",
-  integrations: [tailwind()],
+	integrations: [tailwind()],
 });
